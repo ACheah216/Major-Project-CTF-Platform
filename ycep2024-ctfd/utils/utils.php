a0d676c1df351f0159c7f83da4b1c1484893bccd
ACheah216 <cheah.alex@proton.me>
2024-02-26 22:22:39 +0800

    Update utils.php
    synchronize between local version and docker image version

--- ycep2024-ctfd/utils/utils.php
+++ ycep2024-ctfd/utils/utils.php
@@ -1,9 +1,17 @@
 <?php
-function checkregex($regex_arr, $str){
-    /* checks array of regexes and sees 
-     * if given string matches ANY of them */
+<?php
+function checkregex($regex_arr, $str, $count = 0){
+    /* checks array of regexes 
+
+     * usage: 
+     * $str       -> string to be checked
+     * $regex_arr -> array of regexes for $str to be checked against
+     * $count     -> OPTIONAL. if not specified, 
+     *               assume true on first hit (i.e. count == 0) 
+     * 
+     * https://www.php.net/manual/en/functions.arguments.php */
     foreach ($regex_arr as $regex){
-        if (preg_match($regex, $str) != 0){
+        if (preg_match_all($regex, $str) > $count){
             return TRUE;
         }
     }
